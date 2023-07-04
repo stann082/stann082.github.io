52a68504040e75bbbef3a1d759ca7393f102f364
Stan Bennett <stanley.bennett82@gmail.com>
2023-07-04 15:03:15 -0500

    Commits

---
+++ cli/src/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace serdditcli;
+
+internal class Program
+{
+
+    #region Main Entry Point
+
+    static void Main(string[] args)
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ChangeMeClient", "0.1"));
+
+        var clientId = "YOUR_CLIENT_ID";
+        var secret = "YOUR_SECRET";
+        var bytes = Encoding.ASCII.GetBytes($"{clientId}:{secret}");
+        var clientAuth = new AuthenticationHeaderValue(
+            "Basic", Convert.ToBase64String(bytes)
+        );
+        
+        var postContent = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("grant_type", "password"),
+            new KeyValuePair<string, string>("username", "USERNAME"),
+            new KeyValuePair<string, string>("password", "PASSWORD")
+        });
+        
+        client.DefaultRequestHeaders.Authorization = clientAuth;
+        
+        var response = client.PostAsync("https://www.reddit.com/api/v1/access_token", postContent).GetAwaiter().GetResult();
+        var responseContent = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        Console.WriteLine(responseContent);
+    }
+
+    #endregion
+    
+}
---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Reddit from "./Reddit";
+
+function App() {
+  return (
+    <div>
+      <Reddit />
+    </div>
+  );
+}
+
+export default App;
